6a4425ad7a1c51fd5f18003dfaadf309b8ab106a
Ahmed Al-Nussairi <ahmed@al-nussairi.com>
2026-06-13 13:26:56 +0100

    deploy to netlify

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -1,6 +1,8 @@
+import netlify from '@astrojs/netlify';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  adapter: netlify(),
   vite: {
     optimizeDeps: {
       exclude: ['quickjs-emscripten', '@jitl/quickjs-wasmfile-release-sync'],
